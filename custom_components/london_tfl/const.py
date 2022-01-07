ab10e729f1734f9e9646078e6c3d2510684a4a38
Mihail Morosan <morosanmihail@outlook.com>
2022-01-07 21:40:00 +0000

    - Added roundels from TfL for several transport modes.

--- custom_components/london_tfl/const.py
+++ custom_components/london_tfl/const.py
@@ -9,11 +9,12 @@ CONF_PLATFORM = 'platform'
 CONF_MAX = 'max'
 DEFAULT_MAX = 3
 
-# This is currently dodgy as-is until / unless I get proper permission
-# to use TfL's iconography around this.
 LINE_IMAGES = {
-    'default': '',
-    'dlr': 'http://vignette3.wikia.nocookie.net/locomotive/images/6/66/2000px-DLR_roundel.svg.png/revision/latest?cb=20121228140928',  # noqa
+    'default': 'https://tfl.gov.uk/tfl/common/images/logos/London%20Underground/Roundel/LULRoundel.jpg',  # noqa
+    'dlr': 'https://tfl.gov.uk/tfl/common/images/logos/Docklands%20Light%20Railway/Roundel/DLRRoundel.jpg',  # noqa
+    'london-overground': 'https://tfl.gov.uk/tfl/common/images/logos/London%20Overground/Roundel/OvergroundRoundel.jpg',  # noqa
+    'tram': 'https://tfl.gov.uk/tfl/common/images/logos/London%20Tramlink/Roundel/TramlinkRoundel.jpg',  # noqa
+    'tfl-rail': 'https://tfl.gov.uk/tfl/common/images/logos/TfL%20Rail/Roundel/TfLRailRoundel.jpg',  # noqa
 }
 
 TFL_LINES_URL = 'https://api.tfl.gov.uk/line/mode/tube,dlr,overground,cable-car,tflrail,tram'  # noqa
